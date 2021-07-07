fc856a5d585f68423da9ee6f4fb6ba4342c7b620
Nguyen Cong Vu <nguyencongvu@gmail.com>
2021-07-07 23:33:41 +0700

    Update setup.php

--- setup.php
+++ setup.php
@@ -3,14 +3,14 @@
 $DOMAIN = 'shilenanailsspa'; 
 // branch = main  
 
+
+
 echo 'cd /opt/www/danhba.top/web';
 echo "<br>";
-echo 'rm -rf shilenanailsspa';
-echo "<br>";
+// echo 'rm -rf shilenanailsspa';
+// echo "<br>";
 echo 'git clone -b main https://github.com/WebUp-top/shilenanailsspa.git';
 echo "<br>";
-echo 'usr pwd';
-echo "<br>"; 
 echo 'chmod -R 755 ./shilenanailsspa';
 echo "<br>";
 echo 'chown -R www:www ./shilenanailsspa';
